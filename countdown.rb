37975d8b244c0fb83f8d3bc082e6e717a2776371
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-08-31 20:10:43 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  i = number
+  while i > 0 do
+    puts "#{i.to_s} SECOND(S)!"
+    i -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  i = number
+  while i > 0 do
+    puts "#{i.to_s} SECOND(S)!"
+    i -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
